c4eeeee1ce19aeedea709c90ae0f382ba8976583
Martina Kurkova <martinanoha@seznam.cz>
2025-09-01 15:44:48 +0200

    sluzby

--- js/timeline.js
+++ js/timeline.js
@@ -15,4 +15,45 @@ function DisplayContent() {
       frame.classList.remove("show");
     }
   });
-}
+};
+
+// service
+
+const items = document.querySelectorAll(".service__item");
+
+window.addEventListener("scroll", DisplayItem);
+DisplayItem();
+
+function DisplayItem() {
+  const TriggerBottom = (window.innerHeight / 5) * 4;
+
+  items.forEach((item) => {
+    const topItem = item.getBoundingClientRect().top;
+
+    if (topItem < TriggerBottom) {
+      item.classList.add("show");
+    } else {
+      item.classList.remove("show");
+    }
+  });
+};
+
+// process
+const process = document.querySelectorAll(".process__item");
+
+window.addEventListener("scroll", DisplayProcess);
+DisplayProcess();
+
+function DisplayProcess() {
+  const TriggerBottom = (window.innerHeight / 5) * 4;
+
+  process.forEach((process) => {
+    const topProcess = process.getBoundingClientRect().top;
+
+    if (topProcess < TriggerBottom) {
+      process.classList.add("show");
+    } else {
+      process.classList.remove("show");
+    }
+  });
+};
