618851b884b0d7b23a0a74106aacb92a623db1f7
Martina Kurkova <martinanoha@seznam.cz>
2026-01-16 11:05:35 +0100

    úpravy_ Krocan, Půda

--- dist/images/resize/resize.sh
+++ dist/images/resize/resize.sh
@@ -8,13 +8,13 @@ OUTPUT_DIR="$INPUT_DIR/output"
 mkdir -p "$OUTPUT_DIR/small" "$OUTPUT_DIR/medium" "$OUTPUT_DIR/large" "$OUTPUT_DIR/xl"
 
 # smyčka přes všechny PSD soubory
-for img in "$INPUT_DIR"/*.png; do
-  filename=$(basename "$img" .png)   # název souboru bez přípony
+for img in "$INPUT_DIR"/*.webp; do
+  filename=$(basename "$img" .webp)   # název souboru bez přípony
 
-  convert "$img" -resize 400x -quality 100 "$OUTPUT_DIR/small/${filename}_small_400.png"
-  convert "$img" -resize 600x -quality 100 "$OUTPUT_DIR/medium/${filename}_medium_600.png"
-  convert "$img" -resize 1000x -quality 100 "$OUTPUT_DIR/large/${filename}_large_1000.png"
-  convert "$img" -resize 1200x -quality 100 "$OUTPUT_DIR/xl/${filename}_xl_1200.png"
+  convert "$img" -resize 400x -quality 100 "$OUTPUT_DIR/small/${filename}_small_400.webp"
+  convert "$img" -resize 600x -quality 100 "$OUTPUT_DIR/medium/${filename}_medium_600.webp"
+  convert "$img" -resize 1000x -quality 100 "$OUTPUT_DIR/large/${filename}_large_1000.webp"
+  convert "$img" -resize 1200x -quality 100 "$OUTPUT_DIR/xl/${filename}_xl_1200.webp"
 done
 
 echo "✅ Hotovo! Výsledky jsou ve složce $OUTPUT_DIR"
